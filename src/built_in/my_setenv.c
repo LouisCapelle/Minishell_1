1977eb33cc7f85b7f6cf926821fdde1e46a0809e
Louis CAPELLE <louis.capelle@epitech.eu>
2020-05-12 17:35:17 +0200

    set env is forking great

--- src/built_in/my_setenv.c
+++ src/built_in/my_setenv.c
@@ -32,7 +32,20 @@ int tab_len(char **tab)
 
 char **create_env_variable(char **buffer, shell_t *shell)
 {
-    
+    char **new_env = malloc(sizeof(char *) * tab_len(shell->env) + 2);
+    char *env_variable = NULL;
+    int i = 0;
+
+    while (shell->env[i]) {
+        new_env[i] = my_strdup(shell->env[i]);
+        i += 1;
+    }
+    env_variable = my_strdup(buffer[1]);
+    env_variable = my_strcat(env_variable, "=");
+    env_variable = my_strcat(env_variable, buffer[2]);
+    new_env[i] = my_strdup(env_variable);
+    new_env[i + 1] = NULL;
+    return new_env;
 }
 
 int my_setenv(char **buffer, shell_t *shell)
@@ -40,7 +53,7 @@ int my_setenv(char **buffer, shell_t *shell)
     int args = get_args(buffer);
 
     if (args > 1 && check_creating_env_var(buffer, args) == 0) {
-        create_env_variable(buffer, shell);
+        shell->env = create_env_variable(buffer, shell);
         return 0;
     } else if (args == 1){
         return prompt_env(shell->env);
