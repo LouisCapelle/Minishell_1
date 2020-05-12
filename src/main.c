dc17578ac1e6c530337369f9d3f91716bf68f2eb
Louis CAPELLE <louis.capelle@epitech.eu>
2020-05-12 10:38:52 +0200

    path handling

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 
 int main(int ac, __attribute__((unused))char **av, char **env)
 {
-    if (ac > 2)
+    if (ac > 2 || find_line("PATH=", env) == -1)
         return 84;
     return minishell(env);
 }
