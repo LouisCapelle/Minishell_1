861df3efa16c3e6deb5d5b8606d19014f6b18f9d
Louis CAPELLE <louis.capelle@epitech.eu>
2020-05-12 18:28:44 +0200

    fix crash when checking acces from path

--- src/check/check_access.c
+++ src/check/check_access.c
@@ -32,8 +32,7 @@ char *get_cmd_path(char *cmd, shell_t *shell)
     char *result = NULL;
     int i = 0;
     int success = 0;
-
-    while (success == 0 && i <= count_path_args(shell->path_parsed)) {
+    while (success == 0 && i <= count_path_args(shell->path_parsed) - 1) {
         result = hanled_exec_path(shell->path_parsed[i]+5, cmd);
         if (access(result, 1) != -1)
             success = 1;
